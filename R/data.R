c426dffb4de8fbc81b1b87a225a19f0fb2e7bfc0
Kane <Michael.Kane@ct.gov>
2022-03-16 09:30:44 -0400

    Added documentation for ct_towns data.

--- R/data.R
+++ R/data.R
@@ -1,10 +1,19 @@
 #' @title An sf Object with Connecticut County Shape Objects
 #'
 #' @description
-#' Use this to get ct county shape and other related information.
+#' Use this to get ct county shape files and other related information.
 #' @name ct_county
 #' @docType data
 #' @rdname ct_county
+NULL
+
+#' @title An sf Object with Connecticut County Shape Objects
+#'
+#' @description
+#' Use this to get ct town shape files and other related information.
+#' @name ct_towns
+#' @docType data
+#' @rdname ct_towns
 NULL
 
 #' @title Population Estimates for the US from 2019
